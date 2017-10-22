76d758608ae77a65e4c4e89411eed1548f6ab96a
hugu1026 <hugu1026@gmail.com>
2017-10-22 21:47:25 +0900

    [fix]攻撃時にnull

--- src/main/java/hugu1026/com/github/phantasyweapon/listener/EntityDamage.java
+++ src/main/java/hugu1026/com/github/phantasyweapon/listener/EntityDamage.java
@@ -3,6 +3,7 @@ package hugu1026.com.github.phantasyweapon.listener;
 import hugu1026.com.github.phantasyweapon.event.PhantasyWeaponAttackEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,10 +17,22 @@ public class EntityDamage implements Listener{
     public void EntityDamage(EntityDamageByEntityEvent event){
         if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK
                 && event.getDamager() instanceof Player
-                && event.getEntity() instanceof Creature) {
+                && event.getEntity() instanceof Creature
+                && ((Player) event.getDamager()).getInventory().getItemInMainHand().getType() == Material.IRON_SWORD
+                && ((Player) event.getDamager()).getInventory().getItemInMainHand().getItemMeta().hasLore()
+                && ((Player) event.getDamager()).getInventory().getItemInMainHand().getItemMeta().getLore()
+                .get(0).startsWith(ChatColor.YELLOW + "ジャンル:")) {
+
             Player player = (Player) event.getDamager();
             Creature creature = (Creature) event.getEntity();
-            String type = player.getInventory().getItemInMainHand().getItemMeta().getLore().get(0).replace("ジャンル:", "");
+            String type;
+
+            try {
+                type = player.getInventory().getItemInMainHand().getItemMeta().getLore().get(0).replace("ジャンル:", "");
+            } catch (Exception ex) {
+                return;
+            }
+
             type = ChatColor.stripColor(type);
 
             if(type == null) return;
